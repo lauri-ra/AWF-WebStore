073633df0da09f1e488e6169eae7f6232cabb678
Lauri Rautiala <lauri.rautiala@tuni.fi>
2023-03-01 15:58:26 +0200

    Notification action creator & reducer

--- assignment/frontend/src/redux/actionCreators/notificationsActions.js
+++ assignment/frontend/src/redux/actionCreators/notificationsActions.js
@@ -2,10 +2,7 @@
 
 // NOTIFICATION ACTION CREATORS
 
-import {
-	NEW_NOTIFICATION,
-	REMOVE_NOTIFICATION,
-} from '../../tests/constants/redux.js';
+import { NEW_NOTIFICATION, REMOVE_NOTIFICATION } from '../../tests/constants/redux.js';
 /**
  * @description Action creator that sends a NEW_NOTIFICATION action to the frontends notification-state along with the payload that includes message.
  * @param {Object} newNotification - The notification details
@@ -13,12 +10,19 @@ import {
  * @param {String} newNotification.isSuccess - Tells whether or not it is a succesfull (green) or unsuccessfull (red) message
  * @return {Object} action
  */
-export const createNotification = (
-	newNotification = { message: '', isSuccess: false }
-) => ({});
+export const createNotification = (newNotification = { message: '', isSuccess: false }) => {
+	return {
+		type: NEW_NOTIFICATION,
+		payload: newNotification,
+	};
+};
 
 /**
  * @description Action creator that sends a REMOVE_NOTIFICATION-type action
  * @return {Object} action
  */
-export const removeNotification = () => {};
+export const removeNotification = () => {
+	return {
+		type: REMOVE_NOTIFICATION,
+	};
+};
--- assignment/frontend/src/redux/reducers/notificationReducer.js
+++ assignment/frontend/src/redux/reducers/notificationReducer.js
@@ -1,9 +1,6 @@
 /** @format */
 
-import {
-	NEW_NOTIFICATION,
-	REMOVE_NOTIFICATION,
-} from '../../tests/constants/redux.js';
+import { NEW_NOTIFICATION, REMOVE_NOTIFICATION } from '../../tests/constants/redux.js';
 /**
  * Implement notificationReducer that handles following cases:
  * 1) NEW_NOTIFICATION: adds the new notification to the state.
@@ -12,6 +9,15 @@ import {
  * @param {Object} action the action that calls the reducer.
  * @returns {Object} new state for notification
  */
-const notificationReducer = (state = {}, action) => {};
+const notificationReducer = (state = {}, action) => {
+	switch (action.type) {
+		case NEW_NOTIFICATION:
+			return (state = action.payload);
+		case REMOVE_NOTIFICATION:
+			return (state = {});
+		default:
+			return state;
+	}
+};
 
 export default notificationReducer;
