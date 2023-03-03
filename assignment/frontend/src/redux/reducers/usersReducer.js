323528d5480319883b902f731f03c82cc8ee39af
Lauri Rautiala <lauri.rautiala@tuni.fi>
2023-03-03 16:50:25 +0200

    users reducer

--- assignment/frontend/src/redux/reducers/usersReducer.js
+++ assignment/frontend/src/redux/reducers/usersReducer.js
@@ -19,6 +19,21 @@ import {
  * @param {Object} action the action that calls the reducer.
  * @returns {Array} new state for products
  */
-const usersReducer = (state = [], action) => {};
+const usersReducer = (state = [], action) => {
+	switch (action.type) {
+		case GET_USER:
+			return [...state, action.payload];
+		case GET_USERS:
+			return action.payload;
+		case CLEAR_USERS:
+			return (state = []);
+		case UPDATE_USER:
+			return [...state.filter((user) => user !== action.payload), action.payload];
+		case REMOVE_USER:
+			return state.filter((user) => user.id !== action.payload.id);
+		default:
+			return state;
+	}
+};
 
 export default usersReducer;
