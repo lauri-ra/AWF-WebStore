da9cb28dfb2f1873f7a61636144c472705c3ea66
Lauri Rautiala <lauri.rautiala@tuni.fi>
2023-03-03 14:28:13 +0200

    orders action creator

--- assignment/frontend/src/redux/actionCreators/ordersActions.js
+++ assignment/frontend/src/redux/actionCreators/ordersActions.js
@@ -11,28 +11,91 @@ import {
 } from '../../tests/constants/redux.js';
 import { emptyCart } from './cartActions';
 /**
- * @description Action creator for getting a single order. Dispatches action with type GET_ORDER and payload of the fetched order if succesfull.
- * If the response is not ok, it only dispatches a NEW_NOTIFICATION-type action to the frontends notification state along with the error message from db as an unsuccessfull message.
+ * @description Action creator for getting a single order. Dispatches action with type GET_ORDER
+ * and payload of the fetched order if succesfull.
+ * If the response is not ok, it only dispatches a NEW_NOTIFICATION-type action to the frontends
+ * notification state along with the error message from db as an unsuccessfull message.
  * @param {String} orderId -  The id of the order to get
  * @return {Function} - Thunk -> action
  */
-export const getOrder = (orderId) => {};
+export const getOrder = (orderId) => {
+	return async (dispatch) => {
+		try {
+			const response = await axios.get(`http://localhost:3001/api/orders/${orderId}`);
+			const order = await response.data;
+
+			dispatch({
+				type: GET_ORDER,
+				payload: order,
+			});
+		} catch (error) {
+			dispatch({
+				type: NEW_NOTIFICATION,
+				payload: { isSucess: false, message: error.response.data.error },
+			});
+		}
+	};
+};
 
 /**
- * @description Action creator for getting all orders. Dispatches action with type GET_ORDERS and payload of the fetched orders if succesfull.
- * If the response is not ok, it only dispatches a NEW_NOTIFICATION-type action to the frontends notification state along with the error message from db as an unsuccessfull message.
+ * @description Action creator for getting all orders. Dispatches action with type GET_ORDERS
+ * and payload of the fetched orders if succesfull.
+ * If the response is not ok, it only dispatches a NEW_NOTIFICATION-type action to the frontends
+ * notification state along with the error message from db as an unsuccessfull message.
  * @return {Function} - Thunk -> action
  */
-export const getOrders = () => {};
+export const getOrders = () => {
+	return async (dispatch) => {
+		try {
+			const response = await axios.get('http://localhost:3001/api/orders');
+			const orders = await response.data;
+
+			dispatch({
+				type: GET_ORDERS,
+				payload: orders,
+			});
+		} catch (error) {
+			dispatch({
+				type: NEW_NOTIFICATION,
+				payload: { isSucess: false, message: error.response.data.error },
+			});
+		}
+	};
+};
 
 /**
  * @description Action creator for adding a new order. Dispatches actions:
  * - ADD_ORDER-type with payload that has the new order
  * - EMPTY_CART-type with no payload
  * - NEW_NOTIFICATION with orderMsg.newOrder in the payload
- * If the response is not ok, it only dispatches a NEW_NOTIFICATION-type action to the frontends notification state along with the error message from db as an unsuccessfull message.
+ * If the response is not ok, it only dispatches a NEW_NOTIFICATION-type action to the frontends
+ * notification state along with the error message from db as an unsuccessfull message.
  *
  * @param {Object} newOrder -  The new order to post
  * @return {Function} - Thunk -> action
  */
-export const addOrder = (newOrder) => {};
+export const addOrder = (newOrder) => {
+	return async (dispatch) => {
+		try {
+			const response = await axios.post(`http://localhost:3001/api/orders/`, newOrder);
+			const order = await response.data;
+
+			dispatch({
+				type: ADD_ORDER,
+				payload: order,
+			});
+
+			dispatch(emptyCart());
+
+			dispatch({
+				type: NEW_NOTIFICATION,
+				payload: { isSucess: true, message: orderMsg.newOrder },
+			});
+		} catch (error) {
+			dispatch({
+				type: NEW_NOTIFICATION,
+				payload: { isSucess: false, message: error.response.data.error },
+			});
+		}
+	};
+};
