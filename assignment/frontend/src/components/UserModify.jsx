5233b49c23fbb549d4b81f1258f3ee80cf01ce1f
Lauri Rautiala <lauri.rautiala@tuni.fi>
2023-03-27 14:36:40 +0300

    eslint gymnastics

--- assignment/frontend/src/components/UserModify.jsx
+++ assignment/frontend/src/components/UserModify.jsx
@@ -5,6 +5,52 @@ import { useDispatch, useSelector } from 'react-redux';
 import { dataTestIds } from '../tests/constants/components';
 import { getUser, updateUser } from '../redux/actionCreators/usersActions';
 
+const RoleSelect = ({ selectedOption, handleOptionChange }) => {
+	return (
+		<div data-testid={dataTestIds.selectId.role} className='my-2 flex flex-col border-2 py-3 px-3'>
+			<label>
+				<input
+					type='radio'
+					value='customer'
+					checked={selectedOption === 'customer'}
+					onChange={handleOptionChange}
+				/>
+				Customer
+			</label>
+
+			<label>
+				<input
+					type='radio'
+					value='admin'
+					checked={selectedOption === 'admin'}
+					onChange={handleOptionChange}
+				/>
+				Admin
+			</label>
+		</div>
+	);
+};
+
+const ModfiyButtons = ({ user }) => {
+	return (
+		<div className='my-2'>
+			<button
+				className='mx-2 rounded-md bg-sky-500 px-2 py-1 font-semibold text-white hover:bg-sky-400'
+				type='submit'
+			>
+				Submit
+			</button>
+			<button
+				className='mx-2 rounded-md bg-sky-500 px-2 py-1 font-semibold text-white hover:bg-sky-400'
+				onClick={() => navigate(`/users/${user.id}`)}
+				type='button'
+			>
+				Cancel
+			</button>
+		</div>
+	);
+};
+
 const UserModify = () => {
 	const { id } = useParams();
 	const dispatch = useDispatch();
@@ -20,8 +66,7 @@ const UserModify = () => {
 
 	useEffect(() => {
 		if (users.length === 1) {
-			const user = users[0];
-			setSelectedOption(user.role);
+			setSelectedOption(users[0].role);
 		}
 	}, [users]);
 
@@ -36,11 +81,7 @@ const UserModify = () => {
 	};
 
 	const handleUpdate = () => {
-		const updatedUser = {
-			id: id,
-			role: selectedOption,
-		};
-
+		const updatedUser = { id: id, role: selectedOption };
 		dispatch(updateUser(updatedUser));
 		navigate('/users');
 	};
@@ -54,45 +95,9 @@ const UserModify = () => {
 			<div data-testid={dataTestIds.valueId.name} className='my-2 text-xl font-semibold'>
 				{user.name}
 			</div>
-			<div
-				data-testid={dataTestIds.selectId.role}
-				className='my-2 flex flex-col border-2 py-3 px-3'
-			>
-				<label>
-					<input
-						type='radio'
-						value='customer'
-						checked={selectedOption === 'customer'}
-						onChange={handleOptionChange}
-					/>
-					Customer
-				</label>
 
-				<label>
-					<input
-						type='radio'
-						value='admin'
-						checked={selectedOption === 'admin'}
-						onChange={handleOptionChange}
-					/>
-					Admin
-				</label>
-			</div>
-			<div className='my-2'>
-				<button
-					className='mx-2 rounded-md bg-sky-500 px-2 py-1 font-semibold text-white hover:bg-sky-400'
-					type='submit'
-				>
-					Submit
-				</button>
-				<button
-					className='mx-2 rounded-md bg-sky-500 px-2 py-1 font-semibold text-white hover:bg-sky-400'
-					onClick={() => navigate(`/users/${user.id}`)}
-					type='button'
-				>
-					Cancel
-				</button>
-			</div>
+			<RoleSelect selectedOption={selectedOption} handleOptionChange={handleOptionChange} />
+			<ModfiyButtons user={user} />
 		</form>
 	);
 };
