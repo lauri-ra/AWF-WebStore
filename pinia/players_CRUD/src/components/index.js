69e98c49a548c7a6ef03ae038a52a55c4d6fbdbc
Lauri <91737392+lauri-ra@users.noreply.github.com>
2023-11-14 23:39:56 +0200

    Delete pinia/players_CRUD directory

--- pinia/players_CRUD/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as AddPlayer }  from './AddPlayer.vue'
-export { default as ListPlayers }  from './ListPlayers.vue'
-export { default as Remove } from './Remove.vue'
-export { default as Notification } from './Notification.vue'
-export { default as PiniaLogo } from './PiniaLogo.vue'
--- pinia/players_CRUD/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { createPinia } from 'pinia'
-
-const app = createApp(App).use(createPinia())
-app.mount('#app')
