7d9617405a96a7a5c904244224315feba5db35b7
Lauri <91737392+lauri-ra@users.noreply.github.com>
2023-11-14 23:40:09 +0200

    Delete react directory

--- react/players_CRUD2/src/components/ListPlayer.jsx
+++
@@ -1,9 +0,0 @@
-/**
- * Copy paste your code from the ListPlayer.jsx file here from the previous exercise.
- *
- * @format
- */
-
-export const ListPlayer = ({ name, id, onClick }) => {
-	return 'TODO: ListPlayer';
-};
--- react/players_fetch/src/index.js
+++
@@ -1,8 +0,0 @@
-/** @format */
-
-import App from './App.jsx';
-import { createRoot } from 'react-dom/client';
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
